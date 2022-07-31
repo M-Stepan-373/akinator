9ee1cb23667fa5b203fe1eb78f1b0d9bb4c6cf2b
M-Stepan-373 <step3212m@gmail.com>
2022-07-31 12:48:16 +0300

    Implemented class Akinator

--- Files/akinator.py
+++ Files/akinator.py
@@ -1,10 +1,15 @@
 class Akinator():
     def __init__(self, root: 'Node'):
-        self.root = root
         self.turn = root
 
     def first_turn(self) -> (str, bool):
-        pass
+        if self.turn.definition() is True:
+            return self.turn.question, True
+        else:
+            return self.turn.answer, False
 
     def second_turn(self, answer: bool):
-        pass
+        if answer == True:
+            self.turn = self.turn.right
+        else:
+            self.turn = self.turn.left
