93c2cdb01031c7c32fef7d45ee52c5964457e6aa
M-Stepan-373 <step3212m@gmail.com>
2022-08-02 11:50:31 +0300

    Implemented export

--- Files/shell.py
+++ Files/shell.py
@@ -1,11 +1,11 @@
 def question(text: str) -> bool:
-    if input(text + "?") == "да":
+    if input(text + "?\n") == "да":
         return True
     else:
         return False
 
 def guessing(answer: str) -> (bool, str, str):
-    if input(answer + "?") == "да":
+    if input(answer + "?\n") == "да":
         return True, None, None
     else:
-        return False, input("Что ты загадал?"), input("Чем " + answer + " отличается от вашего ответа?")
+        return False, input("Что ты загадал?\n"), input("Чем " + answer + " отличается от вашего ответа?\n")
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from akinator.Files.tree import Node
 from akinator.Files.akinator import Akinator
 from akinator.Files.shell import question, guessing
+from akinator.Files.base import import_data, export_data
 
 nodes = [Node(0, '', None, None, "Кот")]
 nodes[0].transformation("Оно лает", "Собака", nodes)
+print("Привет, " + input("Как тебя зовут?\n") + ". Я Акинатор, я хочу угадать твоих персонажей, отвечай на вопросы да или нет.")
+
 
 while True:
     akinator = Akinator(nodes[0])
@@ -17,3 +20,7 @@ while True:
             if not yes:
                 akinator.turn.transformation(new_question, new_answer, nodes)
             break
+    if input("Хотите ли вы выйти?\n") == "да":
+        break
+if input("Хотите ли вы сохранить?\n") == "да":
+    export_data('trees/test tree connects.txt', 'trees/test tree.txt', nodes)
